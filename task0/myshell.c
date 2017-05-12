fa812ee0ff42984fdbd6f167797273954c6aedd6
Yuval Levy <yuval.yuki.levy@gmail.com>
2017-05-12 19:24:26 +0300

    basic format of the while loop

--- task0/myshell.c
+++ task0/myshell.c
@@ -1,3 +1,4 @@
+#include <LineParser.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <linux/limits.h>
@@ -5,8 +6,30 @@
 int main (int argc, char* argv[]) {
 
 	char* curDir = NULL;
+	char* user_input = NULL;
+	cmdLine* line = NULL;
 	
 	curDir = getcwd(curDir, PATH_MAX);
 	
+	while (0) {
+		
 		printf("%s>", curDir);
+		fgets(user_input, 2048, stdin);
+		
+		if (strcmp(user_input, "quit") {
+			break;
+		}
+		
+		line = parseCmdLines(user_input);
+		printf("%s/n", line->arguments);
+		execute(line);
+		free(line);
+	}
+	return 0;
+}
+
+void execute(cmdLine* pCmdLine) {
+	
+	
+	
 }

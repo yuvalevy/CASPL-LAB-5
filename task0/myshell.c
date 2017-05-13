ba102f8a5f7a7f151c399a1952bf38ba44e3bd42
levy yuval <leyu@cs302row1-1.cs.bgu.ac.il>
2017-05-13 12:47:33 +0300

    input->cmdLine->free

--- task0/myshell.c
+++ task0/myshell.c
@@ -1,36 +1,40 @@
 #include "LineParser.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <linux/limits.h>
 
+
+void exec_line(cmdLine* line) {
+
+}
+
 int main (int argc, char* argv[]) {
 
 	char* curDir = NULL;
-	char* user_input = NULL;
-	cmdLine* line = NULL;
+	char user_input[2048] = {0};
+	int i = 0;
+  cmdLine* line = NULL; 
 	
-	curDir = getcwd(curDir, PATH_MAX);
-	
-	while (0) {
-		
-		printf("%s>", curDir);
-		fgets(user_input, 2048, stdin);
-		
-		if (strcmp(user_input, "quit")) {
-			break;
-		}
-		
-		line = parseCmdLines(user_input);
-		printf("%s/n", line->arguments);
-		execute(line);
-		free(line);
-	}
+  curDir = getcwd(curDir, PATH_MAX);
+  
+    while(1) {
+  		printf("%s>", curDir);
+  		fgets(user_input, 2048, stdin);
+  
+  		if (0 == strcmp(user_input, "quit\n")) {
+          return 1;
+      }
+  
+   		line = parseCmdLines(user_input);
+      exec_line(line);
+      for ( i = 0 ; i< line->argCount ; i++ ) {
+       		printf("arguments[%d]: %s \n", i, line->arguments[i]);
+      }
+  		free(line);
+    }
 	return 0;
 }
 
-void execute(cmdLine* pCmdLine) {
-	
-	
-	
-}
+

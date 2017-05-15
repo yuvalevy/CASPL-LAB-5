128648097556b8a1990dae84553c42fb33b6740e
levy yuval <leyu@cs302six3-4.cs.bgu.ac.il>
2017-05-15 11:39:13 +0300

    1b realy finished

--- task1/1b/myshell.c
+++ task1/1b/myshell.c
@@ -8,35 +8,43 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+char* strsignal(int);
+int debug = 0;
+
+void print_debug(char * arg) {
+    
+    if(debug == 1) {
+        fprintf(stderr, "%d: running %s\n", getpid() ,arg);
+    }
+
+}
+
 void execute(cmdLine* line) {
-  
-  int execution_result = 0;
+
   pid_t fork_res = fork();
 
-  printf("%d: running %s\n", getpid(), line->arguments[0]);
+  print_debug(line->arguments[0]);
   
   switch(fork_res) {
 
    case -1 :  /* fork faliure */
        
       perror("fork failed\n");
-      execution_result = -1;
+      _exit(1);
       break;   
       
    case 0 : /* child, executing the command */
    
-      execution_result = execvp(line->arguments[0], line->arguments);   
-      break;
+       execvp(line->arguments[0], line->arguments);   
+       perror("execution failed");
+       _exit(1);
+   break;
 	
    default : /* father, waiting for child to finish */
-       
-      waitpid(fork_res, NULL, WUNTRACED);
+       if(line->blocking == 1) {
+           waitpid(fork_res, NULL, WUNTRACED);
+       }
       break; 
-  }
-  
-  if ( -1 == execution_result ) {
-      perror("execution failed");
-      exit(1);
   }
 }
 
@@ -47,13 +55,21 @@ void handle_signal(int signal_num) {
   
 }
 
-int main (int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 
     char* curDir = NULL;
     char user_input[2048] = {0};
     cmdLine* line = NULL;
+    
+    if (argc > 1) {
+     
+        if(0==strcmp(argv[1],"-d")) {
+            debug = 1;
+        }
+    }
+    
     curDir = getcwd(curDir, PATH_MAX);
-
+    
     signal(SIGCHLD, handle_signal);
     signal(SIGQUIT, handle_signal);
     signal(SIGTSTP, handle_signal);
@@ -65,8 +81,20 @@ int main (int argc, char* argv[]) {
         if (0 == strcmp(user_input, "quit\n")) {
             return 0;
         }
-
+        if (0 == strcmp(user_input, "\n")) {
+            continue;
+        }
         line = parseCmdLines(user_input);
+
+        /*printf("line: \n");
+        for ( i = 0 ; i < line->argCount ; i++ ) {
+            printf("arg[%d]: %s\n", i, line->arguments[i]);
+        }
+        printf("inputRedirect: %s\n", line->inputRedirect);
+        printf("outputRedirect: %s\n", line->outputRedirect);        
+        if((line->next)!=NULL) {
+            printf("next: %s\n", line->next->arguments[0]);
+        }*/
         execute(line);
         free(line);
     }
